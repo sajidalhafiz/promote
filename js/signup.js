32afb38662e71c6a59f9091f57617ce13202f026
tryit-100 <safwan.std21@gmail.com>
2023-04-28 19:45:17 +0600

    sign up is done

--- js/signup.js
+++ js/signup.js
@@ -1,1 +1,31 @@
-console.log("hello");
+// select submit button element
+const submit_btn = document.querySelector("#submit");
+
+// perform onclick event on button
+submit_btn.onclick = (e) => {
+    
+    e.preventDefault();
+
+    const uname = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    const pass = document.getElementById("password").value;
+    const cpass = document.getElementById("cpassword").value;
+
+
+    // store input data to the local storage of browser
+    localStorage.setItem("UserName", uname); 
+    localStorage.setItem("Email", email); 
+    localStorage.setItem("Password", pass); 
+    localStorage.setItem("ConfirmPass", cpass);
+    
+    // conditions for input validation
+    if(uname == "" && email == "" && pass == "" && cpass == ""){
+        swal("Oops..!", "Input fields is empty.", "warning"); // warning, info, error, success
+    }else{
+        if(pass !== cpass){
+            swal("Oops..!", "Password is not matching.", "warning");
+        }else{
+            swal("Successful", "Your registration is complete.", "success");
+        }
+    }
+}
