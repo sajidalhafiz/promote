f317aa4e39a2407bd810ff120ff2631bb976e4d3
tryit-100 <safwan.std21@gmail.com>
2023-04-29 20:30:55 +0600

    validation is done

---
+++ js/signin.js
@@ -0,0 +1,26 @@
+const login_btn = document.querySelector("#login");
+
+login_btn.onclick = (e) => {
+
+    e.preventDefault();
+
+    // user login input data
+    const emaill = document.getElementById("email").value;
+    const passs = document.getElementById("password").value;
+
+    // stored login data
+    const s_email = localStorage.getItem("Email");
+    const s_pass = localStorage.getItem("Password");
+
+    // conditions for login
+    if(emaill == "" && passs == ""){
+        swal("Oops..!", "Input fields is empty.", "warning");
+    }else{
+        if(emaill == s_email && passs == s_pass){
+            swal("Successful", "You are signed in.", "success");
+        }else{
+            swal("Oops..!", "Something is wrong.", "warning");
+        }
+    }
+
+}
--- js/signup.js
+++ js/signup.js
@@ -3,7 +3,7 @@ const submit_btn = document.querySelector("#submit");
 
 // perform onclick event on button
 submit_btn.onclick = (e) => {
-    
+
     e.preventDefault();
 
     const uname = document.getElementById("username").value;
@@ -13,19 +13,78 @@ submit_btn.onclick = (e) => {
 
 
     // store input data to the local storage of browser
-    localStorage.setItem("UserName", uname); 
-    localStorage.setItem("Email", email); 
-    localStorage.setItem("Password", pass); 
+    localStorage.setItem("UserName", uname);
+    localStorage.setItem("Email", email);
+    localStorage.setItem("Password", pass);
     localStorage.setItem("ConfirmPass", cpass);
-    
+
+
     // conditions for input validation
-    if(uname == "" && email == "" && pass == "" && cpass == ""){
-        swal("Oops..!", "Input fields is empty.", "warning"); // warning, info, error, success
-    }else{
-        if(pass !== cpass){
-            swal("Oops..!", "Password is not matching.", "warning");
-        }else{
-            swal("Successful", "Your registration is complete.", "success");
+    if (uname === "" && email === "" && pass === "" && cpass === "") {
+        return swal("Oops..!", "Input fields are empty.", "warning"); // warning, info, error, success
+    }
+    else{
+        if(!isValidUser(uname) || uname === ""){
+            swal("Oops..!", "Username is not valid or empty field.", "warning");
         }
+        else if (!isValidEmail(email) || email === "") {
+            swal("Oops..!", "Email is not valid.", "warning");
+        }
+        else if(!isValidPass(pass) || pass === "") {
+            swal("Oops..!", "Password is not valid or empty password field.", "warning");
+        }
+        else if (pass !== cpass) {
+            swal("Oops..!", "Password is not matching.", "warning");
+        }
+        else{
+            return swal("Successful", "Your registration is complete.", "success");
+        }
+    }
+}
+
+// validation code
+
+function isValidUser(input_uname) {
+    const uname_regex = /^[a-z\d_-]{5,12}$/;
+    const uname_parent_div = document.getElementById("username").parentElement;
+
+    if (input_uname.match(uname_regex)) {
+        //success class will be added
+        uname_parent_div.className = 'input-elements success';
+        return true;
+    } else {
+        //error class will be added 
+        uname_parent_div.className = 'input-elements error';
+        return false;
+    }
+}
+
+function isValidEmail(input_email) {
+    const email_regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const email_parent_div = document.getElementById("email").parentElement;
+
+    if (input_email.match(email_regex)) {
+        //success class will be added
+        email_parent_div.className = 'input-elements success';
+        return true;
+    } else {
+        //error class will be added 
+        email_parent_div.className = 'input-elements error';
+        return false;
+    }
+}
+
+function isValidPass(input_pass) {
+    const pass_regex = /^[\d\w@-]{8,20}$/;
+    const pass_parent_div = document.getElementById("password").parentElement;
+
+    if (input_pass.match(pass_regex)) {
+        //success class will be added
+        pass_parent_div.className = 'input-elements success';
+        return true;
+    } else {
+        //error class will be added 
+        pass_parent_div.className = 'input-elements error';
+        return false;
     }
 }
